d9c5c7fe2dca502cdf3a0b816265e557d3c08b4d
roman_awale <roman@soanitech.com>
2023-03-23 17:19:59 +0545

    index is completed

---
+++ js/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Buildiong a payment form
+</body>
+<script>
+    // naming convention
+    // camelCase
+    let firstName = "hem";
+
+    // PascalCase
+    let LastName= "joshi";
+
+
+    // snameCase
+    let middle_name = " Raj ";
+
+    // we cannot use keywors as a vairable name such as let, function,..
+
+    var courseName = "php/laravel";
+
+    var cardNumber = "121343454";
+
+    // payment Status
+    var isPaid = true;
+
+    // declaring a variable
+    var modeOfPayment;
+
+    // taking input from User
+    modeOfPayment = prompt("MOde Of Payment?");
+
+    // output to the user
+    alert(firstName + 'successfully bought' + courseName + "using" + modeOfPayment)
+
+
+
+
+
+</script>
+</html>
